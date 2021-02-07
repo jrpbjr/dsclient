11e2e32cc12d73941ad49f7ebc4aa0c41bb17f98
jrpbjr <jrpbjr@gmail.com>
2021-02-07 19:54:15 -0300

    initial project

---
+++ src/test/java/com/dev/dsclient/DsclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.dsclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
