b17bed24470475ccd0a419f4fba45860f189b8b3
jrpbjr <jrpbjr@gmail.com>
2021-02-08 01:24:30 -0300

    delete client

--- src/main/java/com/dev/dsclient/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/dev/dsclient/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.dev.dsclient.services.exceptions.DatabaseException;
 import com.dev.dsclient.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -16,14 +17,31 @@ public class ResourceExceptionHandler {
 	
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<StandarError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		
 		StandarError err = new StandarError();
 		err.setTimestamp(Instant.now());
-		err.setStatus(HttpStatus.NOT_FOUND.value());
+		err.setStatus(status.value());
 		err.setError("Resource not found");
 		err.setMessage(e.getMessage());
 		err.setPath(request.getRequestURI());
 		
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+		return ResponseEntity.status(status).body(err);
+		
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandarError> database(DatabaseException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		
+		StandarError err = new StandarError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Database exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
 		
 	}
 }
--- src/main/java/com/dev/dsclient/services/ClientService.java
+++ src/main/java/com/dev/dsclient/services/ClientService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dev.dsclient.dto.ClientDTO;
 import com.dev.dsclient.entities.Client;
 import com.dev.dsclient.repositories.ClientRepository;
+import com.dev.dsclient.services.exceptions.DatabaseException;
 import com.dev.dsclient.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -73,6 +76,17 @@ public class ClientService {
 		}catch(EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found" + id);
 		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+		
 	}
 
 }
