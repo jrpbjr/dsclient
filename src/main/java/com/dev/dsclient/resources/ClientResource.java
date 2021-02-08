8856b407881b8f57138a46b76ce671ce8be7acdd
jrpbjr <jrpbjr@gmail.com>
2021-02-08 02:07:55 -0300

    config parameter pageable

--- src/main/java/com/dev/dsclient/resources/ClientResource.java
+++ src/main/java/com/dev/dsclient/resources/ClientResource.java
@@ -31,7 +31,7 @@ public class ClientResource {
 	@GetMapping
 	public ResponseEntity<Page<ClientDTO>> findAll(
 			@RequestParam(value = "page", defaultValue = "0") Integer page,
-			@RequestParam(value = "linesPerPage", defaultValue = "10") Integer linesPerPage,
+			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
 			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
 			@RequestParam(value = "direction", defaultValue = "ASC") String direction			
 			){
