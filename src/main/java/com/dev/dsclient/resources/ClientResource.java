726060a47bc638268d29e2b9c3a215f4a4454d01
jrpbjr <jrpbjr@gmail.com>
2021-02-08 02:00:49 -0300

    pagination

--- src/main/java/com/dev/dsclient/resources/ClientResource.java
+++ src/main/java/com/dev/dsclient/resources/ClientResource.java
@@ -1,9 +1,11 @@
 package com.dev.dsclient.resources;
 
 import java.net.URI;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.dev.dsclient.dto.ClientDTO;
@@ -26,8 +29,16 @@ public class ClientResource {
 	private ClientService service;
 	
 	@GetMapping
-	public ResponseEntity<List<ClientDTO>> findAll(){
-		List<ClientDTO> list = service.findAll();
+	public ResponseEntity<Page<ClientDTO>> findAll(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "10") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction			
+			){
+		
+		PageRequest pageResquest = PageRequest.of(page,linesPerPage, Direction.valueOf(direction), orderBy );
+		
+		Page<ClientDTO> list = service.findAllPaged(pageResquest);
 		return ResponseEntity.ok().body(list);
 	}
 	
